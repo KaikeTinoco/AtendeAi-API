8f66fbdcb1d8a4ed140a6a148c707579ef96e09c
Kaike <kaiketinocon@outlook.com>
2025-08-10 21:17:13 -0300

    criação das entidades

---
+++ main/src/main/java/com/project/app/main/entities/Client.java
@@ -0,0 +1,32 @@
+package com.project.app.main.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.util.List;
+
+@Entity
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @CPF
+    private String cpf;
+    @Email
+    private String email;
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Residence> residences;
+}
---
+++ main/src/main/java/com/project/app/main/entities/Residence.java
@@ -0,0 +1,29 @@
+package com.project.app.main.entities;
+
+import com.project.app.main.enums.State;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Residence {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Client owner;
+
+    private String street;
+    private Integer houseNumber;
+    private String neighboorhood;
+    private String city;
+    private State state;
+
+}
