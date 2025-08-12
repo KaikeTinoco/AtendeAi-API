f920397132685be268a05846be869f5ecfed5fd6
Kaike <kaiketinocon@outlook.com>
2025-08-12 19:46:57 -0300

    mudança de nome de Residence para Location

--- main/src/main/java/com/project/app/main/entities/Location.java
+++ main/src/main/java/com/project/app/main/entities/Location.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Residence {
+public class Location {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
