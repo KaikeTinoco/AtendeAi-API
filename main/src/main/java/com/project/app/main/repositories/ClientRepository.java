07a259e3f1ac898c93af72fab8ee055e91a52591
Kaike <kaiketinocon@outlook.com>
2025-08-14 17:02:11 -0300

    criação do repo para a entidade Client

---
+++ main/src/main/java/com/project/app/main/repositories/ClientRepository.java
@@ -0,0 +1,15 @@
+package com.project.app.main.repositories;
+
+import com.project.app.main.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+    Optional<Client> findById(Long id);
+    Optional<Client> findByCpf(String cpf);
+    Optional<Client> findByNome(String nome);
+    Optional<Client> findByEmail(String email);
+}
