1926d7f9f6072eb0475368570199a82749b13fa4
Kaike <kaiketinocon@outlook.com>
2025-08-14 17:03:22 -0300

    criação do clientDto

---
+++ main/src/main/java/com/project/app/main/dtos/ClientDto.java
@@ -0,0 +1,16 @@
+package com.project.app.main.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ClientDto {
+    private String nome;
+    private String cpf;
+    private String email;
+}
