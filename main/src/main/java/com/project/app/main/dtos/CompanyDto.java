96984f1f6adc1264a0888c6a52779d4e571f3c67
Kaike <kaiketinocon@outlook.com>
2025-08-20 23:46:50 -0300

    criação do dto de cadastro da entidade Company

---
+++ main/src/main/java/com/project/app/main/dtos/CompanyDto.java
@@ -0,0 +1,18 @@
+package com.project.app.main.dtos;
+
+import com.project.app.main.enums.CompanyType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CompanyDto {
+    private String name;
+    private String email;
+    private String cnpj;
+    private CompanyType type;
+}
