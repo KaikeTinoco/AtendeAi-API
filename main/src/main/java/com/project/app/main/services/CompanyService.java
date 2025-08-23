343cac6d0cc20cffd473ed512dbb318f7e337426
Kaike <kaiketinocon@outlook.com>
2025-08-22 23:20:53 -0300

    adição da atualização e delete das empresas

--- main/src/main/java/com/project/app/main/services/CompanyService.java
+++ main/src/main/java/com/project/app/main/services/CompanyService.java
@@ -3,9 +3,12 @@ package com.project.app.main.services;
 import com.project.app.main.dtos.CompanyDto;
 import com.project.app.main.entities.Company;
 import com.project.app.main.exceptions.BadRequestException;
+import com.project.app.main.exceptions.NotFoundException;
 import com.project.app.main.repositories.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CompanyService {
@@ -30,6 +33,33 @@ public class CompanyService {
         repository.save(newCompany);
         return newCompany;
 
-
     }
+
+    public Company updateCompany(Long companyId, CompanyDto dto) {
+        if (companyId == null) {
+            throw new BadRequestException("por favor informe o id da empresa a ser atualizada!");
+        }
+        Company company = repository.findById(companyId).orElseThrow(
+                () -> new NotFoundException("não foi possível encontrar uma empresa com id: " + companyId)
+        );
+        Optional.ofNullable(dto.getCnpj()).ifPresent(company::setCnpj);
+        Optional.ofNullable(dto.getName()).ifPresent(company::setName);
+        Optional.ofNullable(dto.getEmail()).ifPresent(company::setEmail);
+        Optional.ofNullable(dto.getTelefone()).ifPresent(company::setTelefone);
+        repository.save(company);
+        return company;
+    }
+
+    public String deleteCompany(Long companyId){
+        if (companyId == null) {
+            throw new BadRequestException("por favor informe o id da empresa a ser atualizada!");
+        }
+        Company company = repository.findById(companyId).orElseThrow(
+                () -> new NotFoundException("não foi possível encontrar uma empresa com id: " + companyId)
+        );
+        repository.delete(company);
+        return "deletado com sucesso!";
+    }
+
+
 }
