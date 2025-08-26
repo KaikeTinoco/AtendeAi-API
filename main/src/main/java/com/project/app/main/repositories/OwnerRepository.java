a6ef7ae8f19a576928f88a9c9c88b0ec6bcee3b2
Kaike <kaiketinocon@outlook.com>
2025-08-25 23:37:21 -0300

    criação do repositório de Owner

---
+++ main/src/main/java/com/project/app/main/repositories/OwnerRepository.java
@@ -0,0 +1,16 @@
+package com.project.app.main.repositories;
+
+import com.project.app.main.entities.Client;
+import com.project.app.main.entities.Owner;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface OwnerRepository extends JpaRepository<Owner, Long> {
+    Optional<Owner> findById(Long id);
+    Optional<Owner> findByNome(String nome);
+    Optional<Owner> findByEmail(String email);
+    Optional<Owner> findByPhone(String phone);
+}
