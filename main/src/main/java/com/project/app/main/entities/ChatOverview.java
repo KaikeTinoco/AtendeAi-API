45bf3728534263663cceafa2b9e798b8dbdd2436
Kaike <kaiketinocon@outlook.com>
2025-09-08 22:43:56 -0300

    feat: criação da nova entidade de resumo sobre conversas tidas com os clientes

---
+++ main/src/main/java/com/project/app/main/entities/ChatOverview.java
@@ -0,0 +1,28 @@
+package com.project.app.main.entities;
+
+import com.project.app.main.enums.OverviewRating;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Table(name = "chats")
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatOverview {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String overview;
+    private OverviewRating rating;
+    private String nextSugestedAction;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
+}
