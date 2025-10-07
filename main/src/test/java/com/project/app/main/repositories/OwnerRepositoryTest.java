82c5ec68f5da9eb91add3ba98813262193219fc1
Kaike <kaiketinocon@outlook.com>
2025-10-06 23:39:31 -0300

    feat: criação da classe OwnerrepositoryTest

---
+++ main/src/test/java/com/project/app/main/repositories/OwnerRepositoryTest.java
@@ -0,0 +1,24 @@
+package com.project.app.main.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerRepositoryTest {
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByName() {
+    }
+
+    @Test
+    void findByEmail() {
+    }
+
+    @Test
+    void findByPhone() {
+    }
+}
